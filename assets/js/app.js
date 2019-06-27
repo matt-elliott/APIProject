c2fda14a82a660add55a0a3fdcf03e98da5d516f
Matt <mattelliottus@gmail.com>
2019-06-27 13:45:30 -0700

    RND for finding local restaurants

---
+++ assets/js/app.js
@@ -0,0 +1,52 @@
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    var warningView = $('body').attr('<aside>');
+    warningView.attr('class', 'bg-warning text-dark');
+    warningView.html('<h2>Nothing Found!</h2>');
+  }
+}
+
+function showPosition(position) {
+  getData(position);
+}
+
+function getData(position) {
+  var loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+  var map = new google.maps.Map(document.getElementById('map'), { center: loc, zoom: 12 });
+  
+  var input = $('#food-prompt').val();
+  var request = {
+    query: input,
+    location: {
+      lat: 34.0594726,
+      lng: -118.4460542
+    },
+    radius: '500',
+    type: ['restaurant', "food"]
+  };
+
+  var service = new google.maps.places.PlacesService(map);
+  service.textSearch(request, function (response) {
+    buildListView(response);
+  })
+}
+
+function buildListView(res) {
+  res.forEach( function(item) {
+    var html =
+    `
+      <div class="col">
+        <img src="https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png">
+        <h1>${item.name}</h1>
+      </div>
+    `;
+    $('#listView').append(html);
+  });
+}
+
+$('button').on("click", getLocation);
+// $('button').trigger("click");
+// getData();
