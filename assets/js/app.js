6736f2175b9e7013766941c4e8c6efc14a32276d
Matt <mattelliottus@gmail.com>
2019-06-28 13:23:12 -0700

    created restaurants array and populate it with response from google, updated buildListView() to use restaurants array, added data attribute with restaurant index to restaurant title button, data ready to be loaded into single restaurant view

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,65 +1,76 @@
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
-  } else {
-    var warningView = $('body').attr('<aside>');
-    warningView.attr('class', 'bg-warning text-dark');
-    warningView.html('<h2>Nothing Found!</h2>');
-  }
-}
+(function () {
+  var restaurants = [];
 
-function showPosition(position) {
-  getData(position);
-}
-
-function getData(position) {
-  // var loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-  var loc = "Westwood";
-  var map = new google.maps.Map(document.getElementById('map'), { center: {lat: 34.0594726, lng: -118.4460542}, zoom: 12 });
-  
-  var input = $('#food-input').val();
-  var request = {
-    query: input,
-    location: {lat: 34.0594726, lng: -118.4460542},
-    // location: {
-    //   lat: 34.0594726,
-    //   lng: -118.4460542
-    // },
-    radius: 5,
-    type: ['restaurant', "food"]
-  };
-
-  var service = new google.maps.places.PlacesService(map);
-  service.textSearch(request, function (response) {
-    buildListView(response);
-  })
-}
-
-function buildListView(res) {
-  console.log(res);
-  if (res.length === 0) {
-    $('#listView').html(`<div class="error"><h2>Nothing found, squire…</h2>`);
-    return;
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+      var warningView = $('body').attr('<aside>');
+      warningView.attr('class', 'bg-warning text-dark');
+      warningView.html('<h2>Nothing Found!</h2>');
+    }
   }
 
-  $('#listView').empty();
+  function showPosition(position) {
+    getData(position);
+  }
 
-  res.forEach( function(item) {
-    var html =
-    `
-      <div class="col-xs-12 col-md-6">
-        <img src="https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png">
-        <h4>${item.name}</h4>
-        <span class="price-level-${item.price_level}"></span>
-        <span class="${item.opening_hours.open_now}"></span>
-        <span class="${item.rating}"></span>
-      </div>
-    `;
-    $('#listView').append(html);
-    // $('#listView').append(html);
-  });
-}
+  function getData(position) {
+    // var loc = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    var loc = "Westwood";
+    var map = new google.maps.Map(document.getElementById('map'), { center: { lat: 34.0594726, lng: -118.4460542 }, zoom: 12 });
+    
+    var input = $('#food-input').val();
+    var request = {
+      query: input,
+      location: { lat: 34.0594726, lng: -118.4460542 },
+      radius: 15,
+      type: ['restaurant', "food"]
+    };
 
-// $('#button-submit').on("click", getLocation);
-$('#button-submit').on("click", getData);
-$('#button-submit').trigger("click");
+    var service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, function (response) {
+      
+      restaurants = response;
+      
+      buildListView();
+    })
+  }
+
+  function buildListView() {
+    if (restaurants.length === 0) {
+      $('#listView').html(`<div class="error"><h2>Nothing found, squire…</h2>`);
+      return;
+    }
+
+    $('#listView').empty();
+
+    restaurants.forEach(function (item) {
+      var itemID = restaurants.indexOf(item);
+
+      var html =`
+        <div class="col-xs-12 col-md-6">
+          <h4><button class="btn btn-link restaurant-btn"  data-restaurant-id="${itemID}">${item.name}</button></h4>
+          <img src="https://maps.gstatic.com/mapfiles/place_api/icons/geocode-71.png">
+          <span class="price-level-${item.price_level}"></span>
+          <span class="${item.opening_hours.open_now}"></span>
+          <span class="${item.rating}"></span>
+        </div>`;
+
+      $('#listView').append(html);
+    });
+  }
+
+  function loadSingleRestaurantView() {
+    var selectedRestaurantID = $(this).attr('data-restaurant-id');
+    var selectedRestaurant = restaurants[selectedRestaurantID];
+    console.log(selectedRestaurant);
+  }
+
+  // $('#button-submit').on("click", getLocation);
+  $('#button-submit').on("click", getData);
+  $('#button-submit').trigger("click");
+
+  //listeners
+  $(document).on("click", '.restaurant-btn', loadSingleRestaurantView);
+})();
